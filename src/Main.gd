48eac0fc744fb215660809d165b1aa741039845c
Komdog <komdogart@gmail.com>
2022-06-11 10:36:18 -0400

    Updated Updater

--- src/Main.gd
+++ src/Main.gd
@@ -9,14 +9,17 @@ extends Control
 
 func _ready():
 	if $HTTPRequest.connect("request_completed", self, "_on_request_completed") == OK: pass
-	$HTTPRequest.request("https://gitlab.bunfan.com/api/v4/projects/22/repository/tags")
+	$HTTPRequest.request("https://api.github.com/repos/komdog/beat-sync/git/refs/tags")
 
 	
 
 func _on_request_completed(_result, _response_code, _headers, body):
 	var json = JSON.parse(body.get_string_from_utf8())
-	var version_string = JSON.print(json.result[0]['name'], "\t")
-	var version_latest = float( version_string.substr(2) )
+
+	var tag_data = JSON.print(json.result.back()['ref'], "\t")
+	var stripped_tag_data = tag_data.replace('"refs/tags/v', '')
+	var version_latest = float(stripped_tag_data)
+
 	print( "Latest Version: %s" % version_latest )
 	if version_latest > ProjectSettings.get_setting('global/Version'):
 		$Popup/Panel/Label2.text = "Your Version: v%s / Latest v%s" % [ProjectSettings.get_setting('global/Version'), version_latest]
@@ -36,5 +39,5 @@ func _on_Ignore_button_up():
 
 
 func _on_Update_button_up():
-	if OS.shell_open('https://gitlab.bunfan.com/Komdog/beat-sync/-/releases') == OK:
+	if OS.shell_open('https://github.com/komdog/beat-sync/releases') == OK:
 		get_tree().quit()
